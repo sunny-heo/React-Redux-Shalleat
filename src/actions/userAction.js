fd13913beefc24bb1fe23bcefa3543e619a000a2
Sunny Heo <mobileheo@gmail.com>
2018-07-27 14:25:08 -0700

    Add CREATE_USER action

--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -14,3 +14,14 @@ export const fetchUser = () => async dispatch => {
     dispatch({ type: "FETCH_USER_REJECTED", payload: error });
   }
 };
+
+export const createUser = newUser => async dispatch => {
+  dispatch({ type: "CREATE_USER_PENDING" });
+
+  try {
+    const user = await userService.signUp(newUser);
+    dispatch({ type: "CREATE_USER_FULFILLED", payload: user });
+  } catch (error) {
+    dispatch({ type: "CREATE_USER_REJECTED", payload: error });
+  }
+};
