81a2efbd85dcca6d7b4376b08e1ca902cd8ff7e8
Sunny Heo <mobileheo@gmail.com>
2018-07-27 02:04:31 -0700

    Add service Class to abstract fetch requests for user & Finalize redux setup by wrapping App component with Provider and store (#2)

--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -1,10 +1,14 @@
-import userReqeust from "../_request/userReqeust";
+import userService from "../_requests/userService";
 
+const userObj = {
+  email: "sunny@gmail.com",
+  password: "superSecret1@"
+};
 export const fetchUser = () => async dispatch => {
   dispatch({ type: "FETCH_USER_PENDING" });
 
   try {
-    const user = await userReqeust.signIn();
+    const user = await userService.signIn(userObj);
     dispatch({ type: "FETCH_USER_FULFILLED", payload: user });
   } catch (error) {
     dispatch({ type: "FETCH_USER_REJECTED", payload: error });
