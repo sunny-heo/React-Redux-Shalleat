0e605ed24babf2e0c406a448c4a0e9fff90b3f58
Sunny Heo <mobileheo@gmail.com>
2018-07-27 15:55:55 -0700

    Remove useless lines of code

--- src/components/App.js
+++ src/components/App.js
@@ -17,32 +17,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <SignInPage />
-        {/* <button
-          onClick={async e => {
-            const data = await service.post("/restaurants/all", {
-              lat: 49.282205100000006,
-              lng: -123.1084132,
-              radius: 1500
-            });
-            console.log(data);
-          }}
-        >
-          trigeer
-        </button>
-        <button
-          onClick={e => {
-            this.props.dispatch(signInUser());
-          }}
-        >
-          Sign in
-        </button> */}
       </div>
     );
   }
