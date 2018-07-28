61581f72da37e5eb9293ada16aabc7d44b028f0c
Sunny Heo <mobileheo@gmail.com>
2018-07-28 12:50:29 -0700

    Change forms directory to authentications & Add GoogleSignIn & Add authConfig for OAuth

---
+++ src/_config/authConfig.js
@@ -0,0 +1,19 @@
+export default {
+  GOOGLE: {
+    domain: "https://accounts.google.com/o/oauth2/v2/auth",
+    scope:
+      "https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile",
+    accessType: "online",
+    clientId:
+      "648697180446-j8hrcvmuu8avoiferc473fcc821elrll.apps.googleusercontent.com",
+    redirectURL: "https://www.shalleat.com/auth/google",
+    responseType: "token"
+  },
+
+  FACEBOOK: {
+    domain: "https://www.facebook.com/v3.0/dialog/oauth",
+    clientId: "287954711742714",
+    redirectURL: "https://www.shalleat.com/auth/facebook",
+    responseType: "token"
+  }
+};
