f67b7d7df3ee9f9521dfe85fce11adbbefd1e8ae
Sunny Heo <mobileheo@gmail.com>
2018-08-06 12:10:46 -0700

    Remove README

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -15,3 +15,17 @@ const rootReducer = combineReducers({
 });
 
 export default rootReducer;
+
+// window.onbeforeunload = function (e)
+//         {
+
+//             e = e || window.event;
+//             var y = e.pageY || e.clientY;
+//             if (y < 0){
+//             return "Do You really Want to Close the window ?"
+//             }
+//             else {
+//             return "Refreshing this page can result in data loss.";
+//             }
+
+//           }
