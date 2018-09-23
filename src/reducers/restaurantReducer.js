0a5577e87649efc2b0c0046efa465687cdcdb5a5
Sunny Heo <mobileheo@gmail.com>
2018-09-22 17:40:50 -0700

    Implement storing fetched restuarant details

--- src/reducers/restaurantReducer.js
+++ src/reducers/restaurantReducer.js
@@ -22,7 +22,7 @@ const initialState = {
   pendingGetRestaurants: false,
   gotRestaurants: false,
   getRestaurantsError: null,
-  details: {},
+  details: [],
   pendingGetDetails: false,
   gotdetails: false,
   getDetailsError: null,
@@ -59,7 +59,7 @@ export default (state = initialState, action) => {
     }
 
     case RESTAURANT_DETAILS_PENDING: {
-      return { ...state, pendingGetDetails: true };
+      return { ...state, gotdetails: false, pendingGetDetails: true };
     }
 
     case RESTAURANT_DETAILS_REJECTED: {
@@ -75,7 +75,7 @@ export default (state = initialState, action) => {
         ...state,
         pendingGetDetails: false,
         gotdetails: true,
-        details: action.payload
+        details: state.details.concat(action.payload)
       };
     }
 
